567a3a3c4a35e507332282672ea5bc9212d85fbc
Amirtha <amirtha4501@gmail.com>
2022-09-13 22:42:40 +0530

    Added reactive fundamentals - Part 1

--- src/components/Essentials.vue
+++ src/components/Essentials.vue
@@ -1,13 +1,13 @@
 <script setup>
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import WelcomeItem from './WelcomeItem.vue'
-import { ref, reactive } from 'vue'
+import { ref, reactive, nextTick, shallowReactive } from 'vue'
 
 // ref
 const message = ref('This is message!')
 console.log(message.value)
 message.value = 'This is changed message'
-// reactive
+// reactive | Limitations: Works only for object types, connectivity loss
 const counter = reactive({
   count: 0
 })
@@ -37,6 +37,46 @@ function toTitleDate(date = new Date()) {
 function doSomething() {
   console.log("doSomething")
 }
+
+const evtCounter = reactive({ count: 0 })
+function increment() {
+  evtCounter.count++
+}
+
+const tickCounter = ref(0)
+async function incrementTick() {
+  tickCounter.value++
+
+  // DOM not yet updated
+  console.log(document.getElementById('counter').textContent) // 0
+
+  await nextTick()
+  // DOM is now updated
+  console.log(document.getElementById('counter').textContent) // 1
+}
+
+const shallow = shallowReactive({
+  foo: 1,
+  nested: {
+    bar: 2
+  }
+})
+// mutating state's own properties is reactive
+shallow.foo++
+// NOT reactive
+shallow.nested.bar++
+
+const raw = {}
+const proxy = reactive(raw)
+// proxy is NOT equal to the original.
+console.log("proxy 1", proxy === raw) // false
+// calling reactive() on the same object returns the same proxy
+console.log("proxy 2", reactive(raw) === proxy) // true
+// calling reactive() on a proxy returns itself
+console.log("proxy 3", reactive(proxy) === proxy) // true
+proxy.nested = raw
+console.log("proxy 4", proxy.nested === raw) // false
+
 </script>
 
 <template>
@@ -118,6 +158,55 @@ function doSomething() {
 
     <a v-on:[exp.eventName]="doSomething"> eventName </a>
     <!-- shorthand --><a @[exp.eventName]="doSomething"> eventName </a>
+  </WelcomeItem>
+
+  <!-- Reactivity Fundamentals - Declaring Reactive State -->
+  <WelcomeItem>
+    <template #icon><DocumentationIcon /></template>
+    <template #heading>Reactivity Fundamentals - Declaring Reactive State</template>
+
+    <h2>Example</h2>
+    <button @click="increment">
+      {{ evtCounter.count }}
+    </button>
+  </WelcomeItem>
+
+  <!-- Reactivity Fundamentals - DOM Update Timing -->
+  <WelcomeItem>
+    <template #icon><DocumentationIcon /></template>
+    <template #heading>Reactivity Fundamentals - DOM Update Timing</template>
+
+    A utility for waiting for the next DOM update flush.
+    <h2>Example</h2>
+    <button id="counter" @click="incrementTick">{{ tickCounter }}</button>
+  </WelcomeItem>
+
+  <!-- Reactivity Fundamentals - Deep Reactivity -->
+  <WelcomeItem>
+    <template #icon><DocumentationIcon /></template>
+    <template #heading>Reactivity Fundamentals - Deep Reactivity</template>
+
+    Unlike reactive(), there is no deep conversion: only root-level properties are reactive for a shallow reactive object.
+    Property values are stored and exposed as-is - this also means properties with ref values will not be automatically unwrapped.
+    <h2>Example</h2>{{shallow.foo}} - {{shallow.nested.bar}}
+  </WelcomeItem>
+
+  <!-- Reactivity Fundamentals - Reactive Proxy vs. Original -->
+  <WelcomeItem>
+    <template #icon><DocumentationIcon /></template>
+    <template #heading>Reactivity Fundamentals - Reactive Proxy vs. Original</template>
+
+    Returned value from reactive() is a Proxy of the original object, which is not equal to the original object
+  </WelcomeItem>
+
+  <!-- Reactivity Fundamentals - Limitations of reactive() -->
+  <WelcomeItem>
+    <template #icon><DocumentationIcon /></template>
+    <template #heading>Reactivity Fundamentals - Limitations of reactive()</template>
+
+    1. It only works for object types. It cannot hold primitive types such as string, number or boolean.
+    2. When we assign or destructure a reactive object's property into local variables, or when we pass that property into a function, 
+    we will lose the reactivity connection
   </WelcomeItem>
 
 </template>
