2668c1b5f61a12dc4aa68f7c3a3d64168691c7d8
Amirtha <amirtha4501@gmail.com>
2022-10-02 13:39:30 +0530

    Added styles and routes

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,24 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <RouterLink to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </RouterLink>
+      <RouterLink to="/products" class="top-bar-link">
+        <span>Products</span>
+      </RouterLink>
+      <RouterLink to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </RouterLink>
+    </nav>
+    <!-- <a @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </a> -->
   </header>
 
   <RouterView />
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './assets/styles/style.scss'
 
 const app = createApp(App)
 
